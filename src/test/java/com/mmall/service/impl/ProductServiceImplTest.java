f327d025a2aca75f1c4e9202225139007fc02adf
yangnk <yangningkai@qq.com>
2019-01-04 23:35:28 +0800

    新增商品管理模块

---
+++ src/test/java/com/mmall/service/impl/ProductServiceImplTest.java
@@ -0,0 +1,95 @@
+package com.mmall.service.impl;
+
+import org.junit.Test;
+import org.junit.Before; 
+import org.junit.After; 
+
+/** 
+* ProductServiceImpl Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>一月 4, 2019</pre> 
+* @version 1.0 
+*/ 
+public class ProductServiceImplTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: getProductList(int pageNum, int pageSize) 
+* 
+*/ 
+@Test
+public void testGetProductList() throws Exception {
+//TODO: Test goes here...
+} 
+
+/** 
+* 
+* Method: saveOrUpdateProduct(Product product) 
+* 
+*/ 
+@Test
+public void testSaveOrUpdateProduct() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: manageProductDetail(Integer productId) 
+* 
+*/ 
+@Test
+public void testManageProductDetail() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+/** 
+* 
+* Method: assembleProductDetailVo(Product product) 
+* 
+*/ 
+@Test
+public void testAssembleProductDetailVo() throws Exception { 
+//TODO: Test goes here... 
+/* 
+try { 
+   Method method = ProductServiceImpl.getClass().getMethod("assembleProductDetailVo", Product.class); 
+   method.setAccessible(true); 
+   method.invoke(<Object>, <Parameters>); 
+} catch(NoSuchMethodException e) { 
+} catch(IllegalAccessException e) { 
+} catch(InvocationTargetException e) { 
+} 
+*/ 
+} 
+
+/** 
+* 
+* Method: assembleProductListVo(Product product) 
+* 
+*/ 
+@Test
+public void testAssembleProductListVo() throws Exception { 
+//TODO: Test goes here... 
+/* 
+try { 
+   Method method = ProductServiceImpl.getClass().getMethod("assembleProductListVo", Product.class); 
+   method.setAccessible(true); 
+   method.invoke(<Object>, <Parameters>); 
+} catch(NoSuchMethodException e) { 
+} catch(IllegalAccessException e) { 
+} catch(InvocationTargetException e) { 
+} 
+*/ 
+} 
+
+} 
